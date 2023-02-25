20f98d04a242f7fb337032eea01f997742640ced
Kevin <008kevinsil@gmail.com>
2023-02-25 17:16:46 -0500

    Primer Commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs"
+
+
+export const useFetchGifs = ( category) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState( true );
+
+    const getObjectGif = async() =>{
+        const newGifs = await getGifs( category );
+        setImages( newGifs );
+        setIsLoading( false );
+    }
+
+    useEffect( () => {
+        getObjectGif();
+    }, [])
+
+    return{
+        images: images,
+        isLoading: isLoading,
+    }
+}
